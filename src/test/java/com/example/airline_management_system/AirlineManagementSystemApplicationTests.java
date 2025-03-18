56761e11557fb6a8471364be8ab0bcb19c793833
Prathmesh2011 <prathmeshrewatkar2011@gmail.com>
2025-03-18 09:15:45 +0530

    initial commit

---
+++ src/test/java/com/example/airline_management_system/AirlineManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airline_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
